8e93123f94b837da5610d6b5186b8729ce507510
ABIRATA10 <abiratapanda46@gmail.com>
2026-03-21 16:53:19 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -973,7 +973,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 // Railway assigns a dynamic PORT. "0.0.0.0" makes the server public.
-const PORT = process.env.PORT || 3000;
+
 
 app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`🚀 Server is live and public on port ${PORT}`);
